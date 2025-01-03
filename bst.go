a9129377c8c2d1cb2958733977b73b0b65bdce86
Trevor Atkinson <tsa@structx.io>
2025-01-03 16:37:42 -0700

    chore: remove duplicate funcs (#7)

--- bst.go
+++ bst.go
@@ -5,6 +5,11 @@ import (
 	"sync/atomic"
 )
 
+func zeroGN[T any]() T {
+	var t T
+	return t
+}
+
 type node[K cmp.Ordered, T any] struct {
 	key         K
 	payload     T
@@ -39,8 +44,7 @@ func (b *BST[K, T]) Insert(key K, payload T) {
 
 	for {
 
-		c := compare(n.key, nn.key)
-		if c == -1 {
+		if c := cmp.Compare(n.key, nn.key); c == -1 {
 
 			if n.left == nil {
 				n.left = nn
@@ -80,8 +84,8 @@ func (b *BST[K, T]) InsertNode(nn *node[K, T]) {
 
 	for {
 
-		c := compare(n.key, nn.key)
-		if c == -1 {
+		if c := cmp.Compare(n.key, nn.key); c == -1 {
+
 			if n.left == nil {
 				n.left = nn
 				return
@@ -89,6 +93,7 @@ func (b *BST[K, T]) InsertNode(nn *node[K, T]) {
 
 			n = n.left
 			continue
+
 		} else if c == 0 {
 			n.payload = nn.payload
 			return
@@ -107,26 +112,30 @@ func (b *BST[K, T]) InsertNode(nn *node[K, T]) {
 // Search
 func (b *BST[K, T]) Search(key K) (T, error) {
 	if b.head == nil {
-		return zero[T](), ErrNilHead
+		return zeroGN[T](), ErrNilHead
 	}
 
 	n := b.head
 
 	for {
 
-		c := compare(n.key, key)
-		if n.key == key {
+		if c := cmp.Compare(n.key, key); c == 0 {
 			return n.payload, nil
+
 		} else if c == -1 {
+
 			if n.left == nil {
-				return zero[T](), ErrNotFound
+				return zeroGN[T](), ErrNotFound
 			}
+
 			n = n.left
+			continue
 		}
 
 		if n.right == nil {
-			return zero[T](), ErrNotFound
+			return zeroGN[T](), ErrNotFound
 		}
+
 		n = n.right
 	}
 }
